3842b89bf0efe52dc5e6b115d732d810a93248eb
nithinbharathi <nithinbharathik@outlook.com>
2026-07-22 11:41:26 -0300

    added logging

--- backend/inference.py
+++ backend/inference.py
@@ -1,6 +1,7 @@
 import torch
 from model import Bigram, decode
 from pathlib import Path
+import time
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -14,4 +15,6 @@ model.eval()
 
 def infer(max_new_tokens):
     for token in model.generate(torch.zeros((1,1), dtype = torch.long, device = device), max_new_tokens=max_new_tokens):
+        start = time.time()
         yield token
+        print("time after generating the first set of tokens...", time.time()-start)
