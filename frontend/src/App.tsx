f38a012f124e8961fe8e6fb5f097aae637838a1e
nithinbharathi <nithinbharathik@outlook.com>
2026-08-06 10:17:34 -0300

    fix: disable rap button while request is in progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,9 @@ import './App.css'
 import { useState } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL;
+
 async function rap(maxTokens : number, setOutput: (v: string) => void) {
   const res = await fetch(`${API_URL}/rap?max_tokens=${maxTokens}`);
-  console.log(API_URL, res)
-
   const reader = res.body!.getReader();
   const decoder = new TextDecoder();
 
@@ -14,7 +13,6 @@ async function rap(maxTokens : number, setOutput: (v: string) => void) {
   while (true) {
     const { value, done } = await reader.read();
     if (done) break;
-
     output += decoder.decode(value);
     setOutput(output);
   }
@@ -24,11 +22,22 @@ async function rap(maxTokens : number, setOutput: (v: string) => void) {
 function App() {
   const [output, setOutput] = useState("");
   const [tokens, setTokens] = useState(100);
+  const [isRapping, setIsRapping] = useState(false);
 
+  const handleRap = async() => {
+    try{
+      setIsRapping(true)
+      console.log("reached..");
+      await rap(tokens, setOutput)
+    }catch(error){
+      console.error(error)
+    }finally{
+      setIsRapping(false)
+    }
+  }
   return (
     <div className="app">
       <h1 className="title">RapGPT</h1>
-
         <span className="token-label">Max new tokens </span>
         <input
           type="number"
@@ -36,11 +45,10 @@ function App() {
           onChange={e => setTokens(+e.target.value)}
           className="input" />
       <div>
-      <button onClick={() => rap(tokens, setOutput)} className="rap-button">
-        Rap!
+      <button onClick={handleRap} disabled = {isRapping} className="rap-button">
+        {isRapping? "Rapping...": "Rap!"}
       </button>
       </div>
-
       <pre>{output}</pre>
     </div>
   );
